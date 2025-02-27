e6158f5e9830145fb4fb2aa1d363668b3a9c036a
sbx_user1051 <sbx_user1051@169.254.5.223>
2025-02-27 17:28:49 +0000

    Thu Feb 27 17:28:49 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -5,6 +5,11 @@ import io.accelerate.runner.SolutionNotImplementedException;
 public class CheckoutSolution {
 
     public Integer checkout(String skus) {
+
+        // TODO: Not clear from requirements:
+        //      - Structure of the list (comma-separated? - assume one word with unsorted chars)
+        //      - Invalid characters (single characters that are not SKUs, spaces)
+
         throw new SolutionNotImplementedException();
     }
 
--- src/test/java/io/accelerate/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/io/accelerate/solutions/CHK/CheckoutSolutionTest.java
@@ -9,7 +9,43 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class CheckoutSolutionTest {
 
-    @Test
-    void checkout() {
+    private CheckoutSolution checkout;
+
+    @BeforeEach
+    public void setup() {
+        checkout = new CheckoutSolution();
     }
+
+    @Test
+    public void checkout_invalid() {
+        assertThat(checkout.checkout("ABCDE"), equalTo(-1));
+    }
+
+    @Test
+    public void checkout_null() {
+        assertThat(checkout.checkout(null), equalTo(0));
+    }
+
+    @Test
+    public void checkout_empty() {
+        assertThat(checkout.checkout(""), equalTo(0));
+    }
+
+    @Test
+    public void checkout_valid_noOffer() {
+        assertThat(checkout.checkout("ABCD"), equalTo(115));
+    }
+
+    @Test
+    public void checkout_valid_withOffer() {
+        assertThat(checkout.checkout("AAABB"), equalTo(175));
+    }
+
+    @Test
+    public void checkout_valid_overOffer() {
+        int y = (2*130) + (2*50) + (2*45) + 30 + 20 + 15;
+        System.out.println(y);
+        assertThat(checkout.checkout("AAA AAA AA BB BB B CD"), equalTo(515));
+    }
+
 }
