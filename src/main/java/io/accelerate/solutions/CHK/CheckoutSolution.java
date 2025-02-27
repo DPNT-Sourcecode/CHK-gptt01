f5d6573fb2be26997e7ef179373fc84e205881f2
sbx_user1051 <sbx_user1051@169.254.5.223>
2025-02-27 19:32:10 +0000

    Thu Feb 27 19:32:10 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -6,6 +6,34 @@ import java.util.Map;
 
 public class CheckoutSolution {
 
+    private static final String SHOP = """
+            | A    | 50    | 3A for 130, 5A for 200          |
+            | B    | 30    | 2B for 45                       |
+            | C    | 20    |                                 |
+            | D    | 15    |                                 |
+            | E    | 40    | 2E get one B free               |
+            | F    | 10    | 2F get one F free               |
+            | G    | 20    |                                 |
+            | H    | 10    | 5H for 45, 10H for 80           |
+            | I    | 35    |                                 |
+            | J    | 60    |                                 |
+            | K    | 70    | 2K for 120                      |
+            | L    | 90    |                                 |
+            | M    | 15    |                                 |
+            | N    | 40    | 3N get one M free               |
+            | O    | 10    |                                 |
+            | P    | 50    | 5P for 200                      |
+            | Q    | 30    | 3Q for 80                       |
+            | R    | 50    | 3R get one Q free               |
+            | S    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+            | T    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+            | U    | 40    | 3U get one U free               |
+            | V    | 50    | 2V for 90, 3V for 130           |
+            | W    | 20    |                                 |
+            | X    | 17    | buy any 3 of (S,T,X,Y,Z) for 45 |
+            | Y    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+            | Z    | 21    | buy any 3 of (S,T,X,Y,Z) for 45 |""";
+
     private static final Map<Character, Integer> PRICES = new HashMap<>();
     static {
         PRICES.put('A', 50);
@@ -31,9 +59,9 @@ public class CheckoutSolution {
         PRICES.put('U', 40);
         PRICES.put('V', 50);
         PRICES.put('W', 20);
-        PRICES.put('X', 90);
-        PRICES.put('Y', 10);
-        PRICES.put('Z', 50);
+        PRICES.put('X', 17);
+        PRICES.put('Y', 20);
+        PRICES.put('Z', 21);
     }
 
     private static final Map<Character, List<Offer>> OFFERS = Map.of(
@@ -126,3 +154,4 @@ public class CheckoutSolution {
     }
 
 }
+
