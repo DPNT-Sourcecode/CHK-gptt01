f64d72c323f4814e7985cc493acd09dfb3c6afb4
sbx_user1051 <sbx_user1051@169.254.5.223>
2025-02-27 17:49:49 +0000

    Thu Feb 27 17:49:49 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -2,33 +2,57 @@ package io.accelerate.solutions.CHK;
 
 import io.accelerate.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class CheckoutSolution {
 
-    private static Map<Character, Integer> PRICES = Map.of(
+    private static final Map<Character, Integer> PRICES = Map.of(
             'A', 50,
             'B', 30,
             'C', 20,
             'D', 15
     );
 
+    // TODO: Not clear from requirements:
+    //      - Structure of the list (comma-separated? - assume one word with unsorted chars)
+    //      - Invalid characters (single characters that are not SKUs, spaces)
     public Integer checkout(String skus) {
 
+        // Validate SKUs
         skus = skus.toUpperCase();
         if (!isValidSKUs(skus)) return -1;
 
-        // TODO: Not clear from requirements:
-        //      - Structure of the list (comma-separated? - assume one word with unsorted chars)
-        //      - Invalid characters (single characters that are not SKUs, spaces)
+        // Build a map of SKU to count - the 'order'
+        final Map<Character, Integer> order = buildOrderFromSKUs(skus);
 
-        throw new SolutionNotImplementedException();
+        return totalOrder(order);
     }
 
-    private boolean isValidSKUs(final String skus) {
+    private static boolean isValidSKUs(final String skus) {
         return skus.codePoints().allMatch(sku -> PRICES.containsKey((char) sku));
     }
 
+    private static Map<Character, Integer> buildOrderFromSKUs(final String skus) {
+        final Map<Character, Integer> order = new HashMap<>();
+
+        skus.codePoints()
+                .forEach(sku -> order.put(
+                        (char) sku,
+                        order.getOrDefault((char) sku, 0) + 1));
+
+        return order;
+    }
+
+    private static int totalOrder(final Map<Character, Integer> order) {
+        return order.entrySet().stream()
+                .mapToInt(entry ->
+                        PRICES.get(entry.getKey()) * entry.getValue()
+                )
+                .sum();
+    }
+
 }
+
 
 
