12825b6a97dcf546673cb472eefbeabc3a94ba30
sbx_user1051 <sbx_user1051@169.254.5.223>
2025-02-27 19:59:10 +0000

    Thu Feb 27 19:59:10 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -6,62 +6,34 @@ import java.util.Map;
 
 public class CheckoutSolution {
 
-    private static final String SHOP = """
-            | A    | 50    | 3A for 130, 5A for 200          |
-            | B    | 30    | 2B for 45                       |
-            | C    | 20    |                                 |
-            | D    | 15    |                                 |
-            | E    | 40    | 2E get one B free               |
-            | F    | 10    | 2F get one F free               |
-            | G    | 20    |                                 |
-            | H    | 10    | 5H for 45, 10H for 80           |
-            | I    | 35    |                                 |
-            | J    | 60    |                                 |
-            | K    | 70    | 2K for 120                      |
-            | L    | 90    |                                 |
-            | M    | 15    |                                 |
-            | N    | 40    | 3N get one M free               |
-            | O    | 10    |                                 |
-            | P    | 50    | 5P for 200                      |
-            | Q    | 30    | 3Q for 80                       |
-            | R    | 50    | 3R get one Q free               |
-            | S    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
-            | T    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
-            | U    | 40    | 3U get one U free               |
-            | V    | 50    | 2V for 90, 3V for 130           |
-            | W    | 20    |                                 |
-            | X    | 17    | buy any 3 of (S,T,X,Y,Z) for 45 |
-            | Y    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
-            | Z    | 21    | buy any 3 of (S,T,X,Y,Z) for 45 |""";
-
     private static final Map<Character, Integer> PRICES = new HashMap<>();
     static {
-        PRICES.put('A', 50);
-        PRICES.put('B', 30);
-        PRICES.put('C', 20);
-        PRICES.put('D', 15);
-        PRICES.put('E', 40);
-        PRICES.put('F', 10);
-        PRICES.put('G', 20);
-        PRICES.put('H', 10);
-        PRICES.put('I', 35);
-        PRICES.put('J', 60);
-        PRICES.put('K', 80);
-        PRICES.put('L', 90);
-        PRICES.put('M', 15);
-        PRICES.put('N', 40);
-        PRICES.put('O', 10);
-        PRICES.put('P', 50);
-        PRICES.put('Q', 30);
-        PRICES.put('R', 50);
-        PRICES.put('S', 30);
-        PRICES.put('T', 20);
-        PRICES.put('U', 40);
-        PRICES.put('V', 50);
-        PRICES.put('W', 20);
-        PRICES.put('X', 17);
-        PRICES.put('Y', 20);
-        PRICES.put('Z', 21);
+                PRICES.put('A', 50);
+                PRICES.put('B', 30);
+                PRICES.put('C', 20);
+                PRICES.put('D', 15);
+                PRICES.put('E', 40);
+                PRICES.put('F', 10);
+                PRICES.put('G', 20);
+                PRICES.put('H', 10);
+                PRICES.put('I', 35);
+                PRICES.put('J', 60);
+                PRICES.put('K', 70);
+                PRICES.put('L', 90);
+                PRICES.put('M', 15);
+                PRICES.put('N', 40);
+                PRICES.put('O', 10);
+                PRICES.put('P', 50);
+                PRICES.put('Q', 30);
+                PRICES.put('R', 50);
+                PRICES.put('S', 20);
+                PRICES.put('T', 20);
+                PRICES.put('U', 40);
+                PRICES.put('V', 50);
+                PRICES.put('W', 20);
+                PRICES.put('X', 17);
+                PRICES.put('Y', 20);
+                PRICES.put('Z', 21);
     }
 
     private static final Map<Character, List<Offer>> OFFERS = Map.of(
@@ -81,7 +53,7 @@ public class CheckoutSolution {
                     new MultiPurchaseDiscountOffer('H', 5, 45)
             ),
             'K', List.of(
-                    new MultiPurchaseDiscountOffer('K', 2, 150)
+                    new MultiPurchaseDiscountOffer('K', 2, 120)
             ),
             'M', List.of(
                     new GetSomethingFreeOffer('M', 'N', 3)
@@ -141,7 +113,7 @@ public class CheckoutSolution {
     private static int totalOffers(final Map<Character, Integer> order) {
         int total = 0;
 
-        // Loop through the items and apply offers
+        // Loop through the items and apply relevant offers
         for (final Map.Entry<Character, Integer> item : order.entrySet()) {
 
             if (!OFFERS.containsKey(item.getKey())) continue; // No offer for item
@@ -154,3 +126,4 @@ public class CheckoutSolution {
     }
 
 }
+
---
+++ src/main/java/io/accelerate/solutions/CHK/GroupItemDiscountOffer.java
@@ -0,0 +1,10 @@
+package io.accelerate.solutions.CHK;
+
+import java.util.Map;
+
+public class GroupItemDiscountOffer implements Offer {
+    @Override
+    public int apply(Map<Character, Integer> order) {
+        return 0;
+    }
+}
