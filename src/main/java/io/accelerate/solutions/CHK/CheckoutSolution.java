74364b8189ec9954a20f5fdd27ff367a20925199
sbx_user1051 <sbx_user1051@169.254.5.223>
2025-02-27 16:55:49 +0000

    Thu Feb 27 16:55:49 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -3,7 +3,9 @@ package io.accelerate.solutions.CHK;
 import io.accelerate.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
+
     public Integer checkout(String skus) {
         throw new SolutionNotImplementedException();
     }
+
 }
--- src/test/java/io/accelerate/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/io/accelerate/solutions/CHK/CheckoutSolutionTest.java
@@ -1,4 +1,15 @@
-import static org.junit.jupiter.api.Assertions.*;
-class CheckoutSolutionTest {
-  
+package io.accelerate.solutions.CHK;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CheckoutSolutionTest {
+
+    @Test
+    void checkout() {
+    }
 }
